ef1eab4bea10915e6501b02ba899859b77cf3926
Shadab Ahmed <shadab.ansari@gmail.com>
2012-03-22 22:17:57 +0530

    Updated test cases

--- max_queue/spec/max_queue_spec.rb
+++ max_queue/spec/max_queue_spec.rb
@@ -25,4 +25,47 @@ describe MaxQueue do
      end
    end
   end
+
+  describe "#max" do
+    it "should give correct maximum when data is in ascending order" do
+      1.upto(1000) do |i|
+        @queue.queue(i)
+      end
+      1000.times do
+        @queue.max.should eql 1000
+        @queue.dequeue
+      end
+    end
+
+    it "should give correct maximum when data is in ascending order" do
+      1000.downto(1) do |i|
+        @queue.queue(i)
+      end
+      1000.downto(1) do |i|
+        @queue.max.should eql i
+        @queue.dequeue
+      end
+    end
+    
+    it "should give correct maximum when data is repeated" do
+      @queue.queue([1,5]*1000 + [5]*100)
+      until @queue.dequeue
+         @queue.max.should eql 5
+      end
+    end   
+    
+    it "should give correct maximum on random data" do
+      10.times do
+        arr = []
+        1000.times { arr << rand(1000)}
+        @queue.queue arr
+        until arr.empty? do
+          @queue.max.should eql arr.max
+          @queue.dequeue
+          arr.shift
+        end
+      end
+    end
+
+  end
 end
